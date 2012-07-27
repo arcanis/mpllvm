6131f3caa8e0d2639a102e7a0afb81e96ffd2cef
Maël Nison <nison.mael@gmail.com>
2012-07-27 23:54:16 +0200

    Adds a call to reserve()

--- includes/mpllvm.hh
+++ includes/mpllvm.hh
@@ -23,7 +23,7 @@ namespace mpllvm
 
         template < typename T, typename... R >
         struct TypesResolver< T, R... > {
-            static std::vector< llvm::Type * > list( llvm::LLVMContext & llvmContext, int position = 0 )
+            static std::vector< llvm::Type * > && list( llvm::LLVMContext & llvmContext, int position = 0 )
             {
                 std::vector< llvm::Type * > && typeList = mpllvm::internal::TypesResolver< R... >::list( llvmContext, position + 1 );
                 typeList[ position ] = mpllvm::internal::TypeResolver< T >::get( llvmContext );
@@ -33,11 +33,33 @@ namespace mpllvm
 
         template < >
         struct TypesResolver< > {
-            static std::vector < llvm::Type * > list( llvm::LLVMContext &, int = 0 )
+            static std::vector < llvm::Type * > list( llvm::LLVMContext &, int count = 0 )
             {
-                return std::vector< llvm::Type * >( );
+                std::vector< llvm::Type * > vector;
+                vector.resize( count );
+                return vector;
             }
         };
+
+        // Integer type resolver (code factorizing)
+
+        template < typename T >
+        struct IntegerTypeResolver {
+            static llvm::Type * get( llvm::LLVMContext & llvmContext ) {
+                return llvm::IntegerType::get( llvmContext, sizeof( T ) * 8 );
+            }
+        };
+
+        template < > struct TypeResolver< signed char > : public IntegerTypeResolver< signed char > { };
+        template < > struct TypeResolver< unsigned char > : public IntegerTypeResolver< unsigned char > { };
+        template < > struct TypeResolver< signed short > : public IntegerTypeResolver< signed short > { };
+        template < > struct TypeResolver< unsigned short > : public IntegerTypeResolver< unsigned short > { };
+        template < > struct TypeResolver< signed int > : public IntegerTypeResolver< signed int > { };
+        template < > struct TypeResolver< unsigned int > : public IntegerTypeResolver< unsigned int > { };
+        template < > struct TypeResolver< signed long > : public IntegerTypeResolver< signed long > { };
+        template < > struct TypeResolver< unsigned long > : public IntegerTypeResolver< unsigned long > { };
+        template < > struct TypeResolver< signed long long > : public IntegerTypeResolver< signed long long > { };
+        template < > struct TypeResolver< unsigned long long > : public IntegerTypeResolver< unsigned long long > { };
 
         // Basic specializations
 
@@ -45,20 +67,6 @@ namespace mpllvm
         struct TypeResolver< void > {
             static llvm::Type * get( llvm::LLVMContext & llvmContext ) {
                 return llvm::Type::getVoidTy( llvmContext );
-            }
-        };
-
-        template < >
-        struct TypeResolver< int > {
-            static llvm::Type * get( llvm::LLVMContext & llvmContext ) {
-                return llvm::IntegerType::get( llvmContext, sizeof( int ) * 8 );
-            }
-        };
-
-        template < >
-        struct TypeResolver< unsigned int > {
-            static llvm::Type * get( llvm::LLVMContext & llvmContext ) {
-                return llvm::IntegerType::get( llvmContext, sizeof( int ) * 8 );
             }
         };
 
